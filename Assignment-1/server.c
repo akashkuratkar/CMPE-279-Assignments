ad4cc1660d59b9ede5e204671aa07ce13c36e57c
Akash Kuratkar <akashkuratkar@Akashs-New-MacBook-Pro.local>
2021-11-11 18:22:48 -0800

    Removed unwanted variables

--- Assignment-1/server.c
+++ Assignment-1/server.c
@@ -1,92 +1,95 @@
-            // Server side C/C++ program to demonstrate Socket programming
-            #include <unistd.h>
-            #include <stdio.h>
-            #include <sys/socket.h>
-            #include <stdlib.h>
-            #include <netinet/in.h>
-            #include <string.h>
-            #include <pwd.h>
-            #define PORT 8080
+    // Server side C/C++ program to demonstrate Socket programming
+    #include <unistd.h>
+    #include <stdio.h>
+    #include <sys/socket.h>
+    #include <stdlib.h>
+    #include <netinet/in.h>
+    #include <string.h>
+    #include <pwd.h>
+    #define PORT 8080
 
-            int main(int argc, char const *argv[])
+    int main(int argc, char const *argv[])
+    {
+        int server_fd, new_socket, valread;
+        struct sockaddr_in address;
+        int opt = 1;
+        int addrlen = sizeof(address);
+        char buffer[1024] = {0};
+        char *hello = "Hello from server";
+        pid_t split;
+        split = getpid();
+
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        //if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+        //&opt, sizeof(opt)))
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(PORT);
+
+        if (bind(server_fd, (struct sockaddr *)&address,
+                 sizeof(address)) < 0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+        split = fork();
+
+        if (split == 0)
+        {
+            //Getting nobody user information
+            struct passwd *pw = getpwnam("nobody");
+
+            if (pw == NULL)
             {
-                int server_fd, new_socket, valread;
-                struct sockaddr_in address;
-                int opt = 1;
-                int addrlen = sizeof(address);
-                char buffer[1024] = {0};
-                char *hello = "Hello from server";
-                pid_t split;
-                split = getpid();
-                int new_uid;
-          
-                // Creating socket file descriptor
-                if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-                {
-                    perror("socket failed");
-                    exit(EXIT_FAILURE);
-                }
-
-                //if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                //&opt, sizeof(opt)))
-                if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-                {
-                    perror("setsockopt");
-                    exit(EXIT_FAILURE);
-                }
-                address.sin_family = AF_INET;
-                address.sin_addr.s_addr = INADDR_ANY;
-                address.sin_port = htons(PORT);
-       
-                if (bind(server_fd, (struct sockaddr *)&address,
-                         sizeof(address)) < 0)
-                {
-                    perror("bind failed");
-                    exit(EXIT_FAILURE);
-                }
-
-                if (listen(server_fd, 3) < 0)
-                {
-                    perror("listen");
-                    exit(EXIT_FAILURE);
-                }
-
-                 split = fork();
-        
-                if(split==0){
-                //Getting nobody user information  
-                struct passwd *pw = getpwnam("nobody");
-
-               
-                if (pw  == NULL) {
-                    fprintf(stderr, "Get of user information failed.\n"); exit(1);
-                }
-                int new_id = pw->pw_uid;
-                    printf("uid:%d \n",new_id);
-                     if(setuid(new_id) <0){
-	                    printf("Error dropping privilege\n");
-	                    return 0;
-	                }
-                    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                                             (socklen_t *)&addrlen)) < 0)
-                    {
-                        perror("accept");
-                        exit(EXIT_FAILURE);
-                    }
-                    valread = read(new_socket, buffer, 1024);
-                    printf("Read %d bytes: %s\n", valread, buffer);
-                    send(new_socket, hello, strlen(hello), 0);
-                    printf("Hello message sent\n");
-                }else if(split>0){
-                    wait(NULL);
-                    printf("Exiting after returning from child\n");
-                    return 0;
-                }else{
-                    perror("Error occured while forking the process");
-                    _exit(2);
-                }
-
-    
-
+                fprintf(stderr, "Get of user information failed.\n");
+                exit(1);
+            }
+            int new_id = pw->pw_uid;
+            if (setuid(new_id) < 0)
+            {
+                printf("Error dropping privilege\n");
                 return 0;
             }
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                     (socklen_t *)&addrlen)) < 0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+            valread = read(new_socket, buffer, 1024);
+            printf("Read %d bytes: %s\n", valread, buffer);
+            send(new_socket, hello, strlen(hello), 0);
+            printf("Hello message sent\n");
+        }
+        else if (split > 0)
+        {
+            wait(NULL);
+            printf("Exiting after returning from child\n");
+            return 0;
+        }
+        else
+        {
+            perror("Error occured while forking the process");
+            _exit(2);
+        }
+
+        return 0;
+    }
